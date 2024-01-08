b9dfa54af6af845148250fb794f9b316d4389b2a
hiteshendlos <120379241+hiteshendlos@users.noreply.github.com>
2024-01-08 12:22:29 +0530

    final

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const jsonfile = require("jsonfile");
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
+import jsonfile from "jsonfile";
+
 const FILE_PATH = "./hit.json";
-const moment = require("moment");
-const DATE = moment().format();
-const data = { data: DATE };
-const simpleGit = require("simple-git");
 
-jsonfile.writeFile(FILE_PATH, data, (err) => {
-  if (err) {
-    console.error("Error writing to JSON file:", err);
-  } else {
-    console.log("Date has been written to the JSON file successfully.");
-  }
-});
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
 
-simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE });
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+
+  // Generate a random date within the last year, adding weeks and days
+  const DATE = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: DATE,
+  };
+
+  console.log(DATE);
+
+  // Write the data to a JSON file
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    // Add the file, commit with the generated date, and recursively call makeCommit
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
+  });
+};
+
+// Call makeCommit with the desired number of commits (e.g., 500)
+makeCommit(500);
